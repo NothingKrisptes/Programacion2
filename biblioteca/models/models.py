71e85037da51c4416f60da85c30d66279894077e
nothingkrisptes <iancm0168@gmail.com>
2025-11-18 23:04:20 -0500

    Creación del modulo Libro

--- biblioteca/models/models.py
+++ biblioteca/models/models.py
@@ -1,17 +1,28 @@
-# from odoo import models, fields, api
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError, UserError
+from datetime import datetime, timedelta
+import requests
 
+class BibliotecaLibro (models.Model):
+    _name = 'biblioteca.libro'
+    _description = 'Modelo para gestionar libros en una biblioteca'
+    _rec_name = 'nombre_libro'
 
-# class biblioteca(models.Model):
-#     _name = 'biblioteca.biblioteca'
-#     _description = 'biblioteca.biblioteca'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    nombre_libro = fields.Char(string='Nombre del Libro')
+    autor = fields.Char(string='Autor')
+    fecha_publicacion = fields.Date(string='Fecha de Publicación')
+    isbn = fields.Char(string='ISBN')
+    ejemplares_disponibles = fields.Integer(string='Ejemplares Disponibles', default=0)
+    paginas = fields.Integer(string='Número de Páginas')
+    genero = fields.Selection([
+        ('ficcion', 'Ficción'),
+        ('no_ficcion', 'No Ficción'),
+        ('ciencia', 'Ciencia'),
+        ('historia', 'Historia'),
+        ('fantasia', 'Fantasía'),
+        ('biografia', 'Biografía')
+    ], string='Género')
+    descripcion = fields.Text(string='Descripción')
+    costo = fields.Float(string='Costo')
+    editorial = fields.Char(string='Editorial')
 
